6d1a8e5d4aa8a823e93bf235c75a558a40f3da9c
Curt Micol <asenchi@asenchi.com>
2012-01-04 02:16:07 -0500

    Major refactor, cleaner. Needs testing.

--- warfield.go
+++ warfield.go
@@ -4,27 +4,23 @@ import (
 	"fmt"
 	"flag"
 	"net"
+	"io"
 	"os"
 	"godis"
 	"url"
 	"strings"
 	"regexp"
 	"strconv"
+	"bufio"
 )
+
+const Version = `0.0.1`
 
 // flags
 var (
-	laddr = flag.String("l", "127.0.0.1:7777", "the address to listen on")
-	saddr = flag.String("s", "127.0.0.1:514", "the address to send data")
-)
-
-// Our log regex pattern, this can probably be cleaned up quite a bit
-var Re = regexp.MustCompile(`^<([^ ]+)>[0-9] [^ ]+ [a-zA-Z]+ [0-9]+ [^ ]+ [^ ]+ nginx: [^ ]+ [^ ]+ [^ ]+ \[.*\] (\".*\" [0-9]+ [0-9]+ \".*\" \".*\") (.*)$`)
-
-// Two env variables we need to make things work well
-var (
-	cloud    = os.Getenv("CLOUD")
-	redisUrl = os.Getenv("HTTP_REDIS_MASTER_URL")
+	laddr = flag.String("l", "127.0.0.1:7777", "the address to listen on (tcp)")
+	saddr = flag.String("s", "127.0.0.1:514", "the address to send data (udp)")
+	vflag = flag.Bool("v", false, "print version string")
 )
 
 func Usage() {
@@ -32,6 +28,12 @@ func Usage() {
 	fmt.Fprintf(os.Stderr, "\nOptions:\n")
 	flag.PrintDefaults()
 }
+
+// Get CLOUD early and export our regex for testing
+var (
+	cloud = os.Getenv("CLOUD")
+	Re    = regexp.MustCompile(`^<([^ ]+)>[0-9] [^ ]+ [a-zA-Z]+ [0-9]+ [^ ]+ [^ ]+ nginx: [^ ]+ [^ ]+ [^ ]+ \[.*\] (\".*\" [0-9]+ [0-9]+ \".*\" \".*\") (.*)$`)
+)
 
 // Our core log structure
 type LogRecord struct {
@@ -45,32 +47,39 @@ type LogRecord struct {
 
 // Match our domain to app_id and token in Redis
 func (lr *LogRecord) GetToken(r *godis.Client) interface{} {
-	if lr.domain != "" {
-		domainkey := strings.Join([]string{cloud, "domain", lr.domain}, ":")
-		appid, err := r.Hget(domainkey, "app_id")
-		if err != nil {
-			return err
-		}
 
-		lr.appid = appid.String()
+	domainkey := strings.Join([]string{cloud, "domain", lr.domain}, ":")
 
-		appkey := strings.Join([]string{cloud, "app", appid.String()}, ":")
-		token, err := r.Hget(appkey, "heroku_log_token")
-		if err != nil {
-			return err
-		}
-		lr.token = token.String()
-		return nil
+	appid, err := r.Hget(domainkey, "app_id")
+	if err != nil {
+		return err
 	}
-	return os.NewError("requires domain")
+
+	// Save our app_id to our LogRecord
+	lr.appid = appid.String()
+
+	appkey := strings.Join([]string{cloud, "app", appid.String()}, ":")
+
+	token, err := r.Hget(appkey, "heroku_log_token")
+	if err != nil {
+		return err
+	}
+
+	// Save our token to our LogRecord
+	lr.token = token.String()
+	return nil
 }
 
-// The log properly formatted message we send to syslog
+// The log message, properly formatted for us to send to syslog
 func (lr *LogRecord) String() string {
 	return "<" + strconv.Itoa(lr.code) + "> " + lr.token + "[nginx]: " + lr.message
 }
 
 // Our LogRecord cache
+//
+// A mapping of domains->LogRecords. We keep the last 100 around so we don't
+// need to call out to redis each time. We'll probably build a proper cache or
+// use memcache for this at some point.
 type RecordCache struct {
 	domains map[string]LogRecord // An array of LogRecords mapped by domain
 }
@@ -78,18 +87,17 @@ type RecordCache struct {
 // Lookup LogRecords in our cache using domain
 func (rc *RecordCache) GetRecord(domain string) (LogRecord, bool) {
 	record, ok := rc.domains[domain]
-	fmt.Printf("rc-record: %v\n\n\n", record)
 
 	if ok == false {
-		return LogRecord{}, ok
+		// Return an new LogRecord if we don't have a corresponding
+		// record in our cache.
+		return LogRecord{domain: domain}, ok
 	}
 	return record, ok
 }
 
 // Cache logrecords using domain as a key
-func (rc *RecordCache) Cache(lr LogRecord) {
-	fmt.Printf("rc-logrecord: %v\n", lr)
-	fmt.Printf("rc-logrecord.domain: %v\n", lr.domain)
+func (rc *RecordCache) SetRecord(lr LogRecord) {
 	rc.domains[lr.domain] = lr
 }
 
@@ -118,101 +126,125 @@ func main() {
 	flag.Usage = Usage
 	flag.Parse()
 
-	// Setup our UDP connection
-	listen, err := setupReceiver()
-	if err != nil {
-		panic(err)
+	if *vflag {
+		fmt.Println("warfield", Version)
+		return
 	}
 
-	// Serve continuously
-	readReceiverServe(listen)
-}
-
-// We resolve our address provided by -l and listen on that address
-// Return a netConn object
-func setupReceiver() (c *net.UDPConn, err os.Error) {
-	addr, err := net.ResolveUDPAddr("udp", *laddr)
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("Now listening on %v\n", addr)
-	return conn, nil
-}
-
-// Receive a netConn as an argument and pull data off that UDP socket
-// We receive data in 4k chunks and send it to a goroutine that parses the
-// raw log into a LogRecord
-func readReceiverServe(c *net.UDPConn) {
-	for {
-		buf := make([]byte, 4096)
-		fmt.Printf("Size of our buffer: %v\n", len(buf))
-		n, _, err := c.ReadFromUDP(buf)
-		if err == os.EINVAL {
-			panic(err)
-		}
-		if err != nil {
-			fmt.Printf("unable to read buffer: %-v", err)
-			continue
-		}
-
-		fmt.Printf("Number of bytes read: %v\n", n)
-		buf = buf[:n]
-		str := string(buf)
-		fmt.Printf("We are sending: %v\n", str)
-
-		parseRecord(str)
-	}
-	panic("not reached")
-}
-
-// Parse a string and create a LogRecord for it
-func parseRecord(s string) {
 	// Initialize our cache
 	rc := &RecordCache{domains: make(map[string]LogRecord)}
 
-	if redisUrl == "" {
-		redisUrl = "redis://localhost:6379"
+	// env variable containing our redis address
+	var (
+		raddr = os.Getenv("HTTP_REDIS_MASTER_URL")
+	)
+
+	if raddr == "" {
+		raddr = "redis://localhost:6379"
 	}
 
-	redis, err := redisConn(redisUrl)
+	// Open connection to Redis
+	redis, err := redisConn(raddr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	// Resolve our TCPAddress
+	addr, err := net.ResolveTCPAddr("tcp", *laddr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	// Listen on our Addr:Port
+	listen, err := net.ListenTCP("tcp", addr)
+	fmt.Println("Now listening on", *laddr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	// Connect to syslog
+	syslog, err := net.Dial("udp", *saddr)
+	fmt.Println("Now connected to", *saddr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	for {
+		// Accept connections
+		conn, err := listen.AcceptTCP()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		go func() {
+			// Serve forever
+			err := recvServe(conn, syslog, rc, Re, redis)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}()
+	}
+}
+
+func recvServe(r io.Reader, w io.Writer, rc *RecordCache, re *regexp.Regexp, redis *godis.Client) os.Error {
+	nr, err := bufio.NewReaderSize(r, 4096)
 	if err != nil {
 		panic(err)
 	}
 
-	groups := Re.FindStringSubmatch(s)
-	if len(groups) == 0 {
-		panic(err)
-	}
-	// convert our code to an int
-	code, _ := strconv.Atoi(groups[1])
-
-	// groups[3] is 'domain'
-	if groups[3] == "-" || groups[3] == "" {
-		panic(err)
-	}
-
-	// Pull our record if we already have it cached
-	record, ok := rc.GetRecord(groups[3])
-	if ok == false {
-		// create basic record
-		record.raw = s
-		record.token = ""
-		record.appid = ""
-		record.code = code
-		record.message = groups[2]
-		record.domain = groups[3]
-
-		// Get our logrecords token
-		err := record.GetToken(redis)
+	for {
+		line, _, err := nr.ReadLine()
 		if err != nil {
-			panic(err)
+			fmt.Fprintln(os.Stderr, err)
+			continue
 		}
-		// Cache our LogRecord
-		rc.Cache(record)
+
+		// Cast the buffer to a string
+		groups := re.FindStringSubmatch(string(line))
+		// If we didn't match anything, grab next line
+		if len(groups) == 0 {
+			continue
+		}
+
+		// convert our code to an int
+		code, _ := strconv.Atoi(groups[1])
+
+		// groups[3] is 'domain'
+		// If we don't have a domain, can't do anything, next
+		if groups[3] == "-" || groups[3] == "" {
+			continue
+		}
+
+		// Check our cache for our 'domain', return the corresponding LogRecord
+		// or create a new one with 'domain' set.
+		record, ok := rc.GetRecord(groups[3])
+		if ok == false {
+			// Finish filling out the new LogRecord
+			record.raw = string(line)
+			record.token = ""
+			record.appid = ""
+			record.code = code
+			record.message = groups[2]
+
+			// Match our LogRecord with tokens stored in Redis
+			err := record.GetToken(redis)
+			if err != nil {
+				panic(err)
+			}
+			// Cache our LogRecord
+			rc.SetRecord(record)
+		}
+
+		// Send to syslog
+		_, err = w.Write([]byte(record.String()))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 	}
 
-	fmt.Printf("\nRecord: %+v\n\n\n", record.String())
-	// spit log.String() to syslog
-	os.Exit(0)
+	panic("not reached")
 }
